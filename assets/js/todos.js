d763009bfac0499faa2df6fc763df72377b9c17f
prakhar308 <khandelwalprakhar30@gmail.com>
2018-12-27 18:40:48 +0530

    Add styling to page

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -16,6 +16,10 @@ $("input[type = 'text']").keypress(function(event){
 	if(event.which === 13){
 		var newToDo = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X</span> " + newToDo + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + newToDo + "</li>");
 	}
+});
+
+$(".fa-plus-circle").click(function(){
+	$("input[type = 'text']").fadeToggle();
 });
