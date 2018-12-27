e143dbde02e32e307f936a64660bd2ed14af687d
prakhar308 <khandelwalprakhar30@gmail.com>
2018-12-27 13:01:23 +0530

    add new todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,21 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+//add new todo to list
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+		var newToDo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + newToDo + "</li>");
+	}
 });
